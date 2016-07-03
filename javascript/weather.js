90f98dd35bd35ee5411b0f127b0ad0878eb225d2
voodoo15 <arnold.redoblado@gmail.com>
2016-07-03 14:57:07 -0400

    Added the pages and routes to the app

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,1 +1,25 @@
-var weather = angular.module('weather', ['ngRoute', 'ngResource'])
+var weather = angular.module('weather', ['ngRoute', 'ngResource']);
+
+weather.config(['$routeProvider', '$locationProvider', function ($routeProvider,  $locationProvider) {
+
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller:  'homeController'
+  })
+
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.html',
+    controller:  'forecastController'
+  })
+
+}]);
+
+weather.controller('homeController', ['$scope', '$resource', function($scope, $resource) {
+
+}]);
+
+weather.controller('forecastController', ['$scope', '$resource', function($scope, $resource) {
+
+}]);
