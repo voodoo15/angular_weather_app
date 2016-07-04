63689262b68f4a6ffd4edb9660d8a3dd4039079c
voodoo15 <arnold.redoblado@gmail.com>
2016-07-03 22:22:14 -0400

    Pulled data from Open Weather Map API and into my forecast page.  Removed my api key so it won't work unless it's entered. Need to create a function to change the temperature to celsius.

--- javascript/weather.js
+++ javascript/weather.js
@@ -30,7 +30,7 @@ weather.service('searchCityService', function() {
 
 // CONTROLLERS
 
-weather.controller('homeController', ['$scope', '$resource', 'searchCityService', function($scope, $resource, searchCityService) {
+weather.controller('homeController', ['$scope', 'searchCityService', function($scope, searchCityService) {
 
   $scope.city = searchCityService.city;
 
@@ -44,10 +44,12 @@ weather.controller('homeController', ['$scope', '$resource', 'searchCityService'
 
 weather.controller('forecastController', ['$scope', '$resource', 'searchCityService', function($scope, $resource, searchCityService) {
 
-  $scope.$watch('city', function() {
+  $scope.city = searchCityService.city;
 
-    $scope.city = searchCityService.city;
+  var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=X', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
 
-  });
+  $scope.weatherResult = weatherAPI.get({q: $scope.city, cnt: 5});
+
+  console.log($scope.weatherResult)
 
 }]);
